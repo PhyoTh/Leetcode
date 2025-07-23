f131ad93aec1f4c5e85005bdfc6f37ea13eec784
Phyo Thant <phyothant2593@gmail.com>
2025-07-23 13:18:32 -0700

    [271] [Time Beats: 11.32%] [Memory Beats: 75.43%] - LeetPush

--- Encode-and-Decode-Strings.py
+++ Encode-and-Decode-Strings.py
@@ -1,32 +1,27 @@
 class Codec:
-
     def encode(self, strs):
-        encoded_string = ''
-
+        encoded_str = ''
         for s in strs:
-            encoded_string += s.replace('/', '//') + '/:'
-        return encoded_string
+            temp = s.replace('/', '//') + '/:'
+            encoded_str = encoded_str + temp # join is used to join the list of str with the "-".join, - as dilimeter
+        return encoded_str
 
     def decode(self, s):
-        decoded_string = []
-        
-        current_string = ''
+        decoded_strs = []
+        temp = ''
         i = 0
-        while i < len(s):
-            # for the actual case of splitting
-            if s[i: i+2] == '/:':
-                decoded_string.append(current_string)
-                current_string = ''
+        while i < len(s): # for loop with range doesn't increment like c++
+            if s[i] == '/' and i+1 < len(s) and s[i+1] == '/':
+                temp = temp + '/'
                 i += 2
-            # for the case where we escape
-            elif s[i: i+2] == '//':
-                current_string += '/'
+            elif s[i] == '/' and i+1 < len(s) and s[i+1] == ':':
+                decoded_strs.append(temp)
+                temp = ''
                 i += 2
             else:
-                current_string += s[i]
+                temp = temp + s[i]
                 i += 1
-            
-        return decoded_string
+        return decoded_strs
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
