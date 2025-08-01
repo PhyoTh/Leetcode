dfb8fd4d224d62da4c212a01bae47756eca6e8e0
Phyo Thant <phyothant2593@gmail.com>
2025-08-01 15:46:41 -0700

    [259] [Time Beats: 53.57%] [Memory Beats: 69.05%] - LeetPush

--- 3Sum-Smaller.py
+++ 3Sum-Smaller.py
@@ -1,29 +1,24 @@
 class Solution(object):
     def threeSumSmaller(self, nums, target):
-        # 2 pointer approach
-        threeSumCount = 0
-        nums.sort() # we need to sort this so we can satisfy i<j<k condition
+        counter = 0
+        nums.sort()
 
         for i in range(len(nums)):
-            # if target is negative, and the sorted array starts with positive
-            # there is no possible tuple that will get to negative target
-            if target <= 0 and nums[i] > 0: 
+            # early break: if the target is negative and 
+            # the sorted array starts with positive
+            # it's impossible to get the sum of negative value
+            if target <= 0 and nums[i] > 0:
                 break
-            threeSumCount = self.twoSum(nums, i, target, threeSumCount)
-        return threeSumCount
-    
-    def twoSum(self, nums, i, target, threeSumCount):
-        low = i + 1
-        high = len(nums) - 1
 
-        while low < high:
-            check = nums[i] + nums[low] + nums[high]
-            if check < target:
-                # trick: because the high is at the highest max you can obtain
-                # so even if you high -= 1, with the same i, and low
-                # you [i] + [low] + [high] will still < target
-                threeSumCount += (high - low)
-                low += 1
-            else:
-                high -= 1 # sum is too big, need to reduce down
-        return threeSumCount
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                threeSum = nums[i] + nums[left] + nums[right]
+                if threeSum < target:
+                    counter += right - left
+                    left += 1
+                else:
+                    right -= 1
+                        
+        return counter
