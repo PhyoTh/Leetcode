5de13af71d54f39f736f78c6cd516bdf948c21f1
Phyo Thant <phyothant2593@gmail.com>
2025-08-01 15:31:44 -0700

    [15] [Time Beats: 87.97%] [Memory Beats: 62.96%] - LeetPush

--- 3Sum.py
+++ 3Sum.py
@@ -1,29 +1,35 @@
 class Solution(object):
     def threeSum(self, nums):
-        # 2 pointer approach
+        # practice
         res = []
         nums.sort()
+
         for i in range(len(nums)):
-            if nums[i] > 0:
+            # early break case: if there is no neg, then impossible to get to zero
+            if nums[i] > 0: 
                 break
-            elif i == 0 or nums[i] != nums[i-1]:
-                self.twoSumII(nums, i, res)
+            elif i == 0 or nums[i] != nums[i-1]: # 1st loop or skip pivot duplicates 
+                # 2pointer operation to find 3sum
+                left = i + 1
+                right = len(nums) - 1
+
+                while left < right:
+                    threeSum = nums[i] + nums[left] + nums[right]
+                    if threeSum < 0:
+                        left += 1
+                    elif threeSum > 0:
+                        right -= 1
+                    else:
+                        res.append([nums[i], nums[left], nums[right]])
+                        # these are just optimizations purpose, not really needed
+                        # to skip the inner dups
+                        while left + 1 < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right - 1 and nums[right] == nums[right-1]:
+                            right -= 1
+                        # you need to move both of the pointers regardless 
+                        # because you want all the unqie pair of left and right
+                        # corresponding to the pivot pointer
+                        left += 1
+                        right -= 1
         return res
-
-    
-    def twoSumII(self, nums, i, res):
-        low = i + 1
-        high = len(nums) - 1
-
-        while low < high:
-            check = nums[i] + nums[low] + nums[high]
-            if check < 0:
-                low += 1
-            elif check > 0:
-                high -= 1
-            else:
-                res.append([nums[i], nums[low], nums[high]])
-                low += 1
-                high -= 1
-                while low < high and nums[low] == nums[low - 1]:
-                    low += 1
