ffe218f96955508ae9d6c90b31e5f20f90822dd2
Phyo Thant <phyothant2593@gmail.com>
2025-08-04 16:49:23 -0700

    [424] [Time Beats: 93.35%] [Memory Beats: 73.02%] - LeetPush

--- Longest-Repeating-Character-Replacement.py
+++ Longest-Repeating-Character-Replacement.py
@@ -1,26 +1,24 @@
 class Solution(object):
     def characterReplacement(self, s, k):
-        # sliding window slow, without frequency map
-        # this algorithm marks each unique char in the string as fixed 
-        # like an idiodt and loop over each unique char
-        letters_set = set(s) # so it only contains the unique chars without dups
-        max_window = 0
+        # sliding window fast
+        # keep in mind that max_window never shrinks, it only grows to
+        # end - start + 1, and we move end / start pointers according to the 
+        # max_freq we have seen through out the whole string
+        max_window = 0 
 
-        for letter in letters_set:
-            count = 0
-            start = 0 
-            for end in range(len(s)):
-                # count up the unique letter when expanding the end of window
-                if s[end] == letter:
-                    count += 1
+        freq_map = {}
+        max_freq = 0
 
-                # end-start+1 = window
-                # as long as window - fixed_letter_count is > k, we need to shrink
-                while end-start+1 - count > k:
-                    if s[start] == letter:
-                        count -= 1
-                    start += 1
-                    
-                max_window = max(max_window, end-start+1)
-        
+        start = 0
+        for end in range(len(s)):
+            freq_map[s[end]] = freq_map.get(s[end], 0) + 1
+            # important note: this is max_freq we have seen in any window
+            max_freq = max(max_freq, freq_map[s[end]])
+
+            if end-start+1 - max_freq > k:
+                freq_map[s[start]] -= 1
+                start += 1
+
+            max_window = end - start + 1
+            
         return max_window
