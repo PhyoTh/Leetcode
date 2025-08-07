c0fe66ea5df6ef6d85be0f37ce832472a22667ac
Phyo Thant <phyothant2593@gmail.com>
2025-08-06 17:53:17 -0700

    [424] [Time Beats: 92.16%] [Memory Beats: 73.48%] - LeetPush

--- Longest-Repeating-Character-Replacement.py
+++ Longest-Repeating-Character-Replacement.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def characterReplacement(self, s, k):
-        # sliding window fast
-        # keep in mind that max_window never shrinks, it only grows to
-        # end - start + 1, and we move end / start pointers according to the 
-        # max_freq we have seen through out the whole string
-        max_window = 0 
+        max_window = 0
 
         freq_map = {}
         max_freq = 0
@@ -12,13 +8,12 @@ class Solution(object):
         start = 0
         for end in range(len(s)):
             freq_map[s[end]] = freq_map.get(s[end], 0) + 1
-            # important note: this is max_freq we have seen in any window
-            max_freq = max(max_freq, freq_map[s[end]])
+            max_freq = max(max_freq, freq_map[s[end]]) # this is all time max freq
 
-            if end-start+1 - max_freq > k:
-                freq_map[s[start]] -= 1
-                start += 1
+            if end-start+1 - max_freq > k: # current window has > k replacements
+                freq_map[s[start]] -= 1 # update the frequency
+                start += 1 # move the window without shrinking
 
-            max_window = end - start + 1
-            
+            max_window = end - start + 1 # max_window will be the current window because we didn't shrink
+        
         return max_window
