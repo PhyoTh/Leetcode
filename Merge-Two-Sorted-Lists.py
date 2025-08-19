83d36795382b668b8758e15ba24f1ab5f0e01d6d
Phyo Thant <phyothant2593@gmail.com>
2025-08-19 14:37:58 -0700

    [21] [Time Beats: 100.00%] [Memory Beats: 97.56%] - LeetPush

--- Merge-Two-Sorted-Lists.py
+++ Merge-Two-Sorted-Lists.py
@@ -5,14 +5,19 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        # recurssive solution
-        if list1 is None:
-            return list2
-        elif list2 is None:
-            return list1
-        elif list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
+        # iteration solution
+        result = ListNode(-1) # this is the way python create a null node
+        prev = result
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+            
+        prev.next = list1 if list1 is not None else list2
+
+        return result.next
