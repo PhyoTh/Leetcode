eec9e3aabf66932bfb14b6fce73ec22952b67fc4
Phyo Thant <phyothant2593@gmail.com>
2025-08-19 14:32:42 -0700

    [21] [Time Beats: 32.15%] [Memory Beats: 46.62%] - LeetPush

---
+++ Merge-Two-Sorted-Lists.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        # recurssive solution
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
