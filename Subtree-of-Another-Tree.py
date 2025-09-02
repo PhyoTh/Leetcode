755c924950a49fa5c57edd7367c46c160417bca2
Phyo Thant <phyothant2593@gmail.com>
2025-09-02 15:10:33 -0700

    [572] [Time Beats: 14.06%] [Memory Beats: 54.22%] - LeetPush

---
+++ Subtree-of-Another-Tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        if subRoot is None:
+            return True
+        elif root is None:
+            return False
+        elif self.sameTree(root, subRoot):
+            return True
+        
+        left = self.isSubtree(root.left, subRoot)
+        right = self.isSubtree(root.right, subRoot)
+        return left or right
+
+    def sameTree(self, root1, root2):
+        if root1 is None and root2 is None:
+            return True
+        elif root1 is None or root2 is None:
+            return False
+        elif root1.val != root2.val:
+            return False
+        
+        left = self.sameTree(root1.left, root2.left)
+        right = self.sameTree(root1.right, root2.right)
+        return left and right
