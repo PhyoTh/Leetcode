4ee81033fd38c3981dfbea500d7d8eda3b717a60
Phyo Thant <phyothant2593@gmail.com>
2025-09-03 14:40:54 -0700

    [235] [Time Beats: 58.55%] [Memory Beats: 35.46%] - LeetPush

--- Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -7,9 +7,9 @@
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        if p.val < root.val and q.val < root.val:
-            return self.lowestCommonAncestor(root.left, p, q)
-        elif p.val > root.val and q.val > root.val:
+        if p.val > root.val and q.val > root.val: # search on the right subTree
             return self.lowestCommonAncestor(root.right, p, q)
-        else:
+        elif p.val < root.val and q.val < root.val: # serach on the left subTree
+            return self.lowestCommonAncestor(root.left, p, q)
+        else: # we have reached the divider and the root is the LCA
             return root
