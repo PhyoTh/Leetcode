33ba6ed538d4598824837c3617a3540aca1e8591
Phyo Thant <phyothant2593@gmail.com>
2025-10-23 15:26:45 -0700

    [225] [Time Beats: 100.00%] [Memory Beats: 22.11%] - LeetPush

---
+++ Implement-Stack-using-Queues.py
@@ -0,0 +1,26 @@
+from collections import deque
+class MyStack:
+    def __init__(self):
+        self.myStack = deque()
+
+    def push(self, x: int) -> None:
+        self.myStack.append(x)
+
+    def pop(self) -> int:
+        if self.empty():
+            return None
+        return self.myStack.pop()
+
+    def top(self) -> int:
+        return self.myStack[-1]
+
+    def empty(self) -> bool:
+        return len(self.myStack) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
