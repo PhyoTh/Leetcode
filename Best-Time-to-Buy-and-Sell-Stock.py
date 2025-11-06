fe6db5099418e6336958f627af6b21d2112d4811
Phyo Thant <phyothant2593@gmail.com>
2025-11-05 16:24:58 -0800

    [121] [Time Beats: 31.24%] [Memory Beats: 32.91%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.py
+++ Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,16 +1,10 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        min_buy_rate = float('inf')
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_buy = float('inf')
         max_profit = 0
         
         for price in prices:
-            min_buy_rate = min(min_buy_rate, price)
-            max_profit = max(max_profit, price - min_buy_rate)
-
+            min_buy = min(min_buy, price)
+            max_profit = max(max_profit, price - min_buy)
+        
         return max_profit
-'''
-[7, 1, 5, 3, 6, 4]
-             |
-min_buy = 1
-max_profit = 5
-'''
