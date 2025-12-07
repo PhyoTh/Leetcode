8f5c431173fe401305be92fac8e290e14c313989
Phyo Thant <phyothant2593@gmail.com>
2025-12-07 14:53:42 -0800

    [79] [Time Beats: 42.02%] [Memory Beats: 21.53%] - LeetPush

--- Word-Search.py
+++ Word-Search.py
@@ -1,25 +1,25 @@
-class Solution(object):
-    def exist(self, board, word):
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                if word[0] == board[row][col] and self.hasWordOnBoard(board, row, col, 0, word):
-                    return True
-
-        return False
-
-    def hasWordOnBoard(self, board, row, col, k, word):
-        if len(word) == k:
-            return True
-        if not(0 <= row < len(board) and 0 <= col < len(board[0])) or board[row][col] != word[k]:
-            return False
-
-        temp = board[row][col]
-        board[row][col] = "#"
-
-        found = (self.hasWordOnBoard(board, row - 1, col, k+1, word) or
-                 self.hasWordOnBoard(board, row + 1, col, k+1, word) or 
-                 self.hasWordOnBoard(board, row, col + 1, k+1, word) or 
-                 self.hasWordOnBoard(board, row, col - 1, k+1, word))
-
-        board[row][col] = temp
-        return found
+1class Solution:
+2    def exist(self, board: List[List[str]], word: str) -> bool:
+3        m = len(board)
+4        n = len(board[0])
+5
+6        def hasWord(row, col, word, visited):
+7            if len(word) == 0:
+8                return True
+9
+10            for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]: # right down left top
+11                dx, dy = row + x, col + y
+12                if 0 <= dx < m and 0 <= dy < n:
+13                    if (dx, dy) not in visited and board[dx][dy] == word[0]:
+14                        visited.add((dx, dy))
+15                        if hasWord(dx, dy, word[1:], visited):
+16                            return True
+17                        visited.remove((dx, dy))
+18            return False
+19
+20        for i in range(m):
+21            for j in range(n):
+22                if board[i][j] == word[0] and hasWord(i, j, word[1:], set([(i, j)])):
+23                    return True
+24
+25        return False
