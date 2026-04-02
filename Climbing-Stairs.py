f228ac625669cfdf2b2ad7809a3252f509ac544a
Phyo Thant <phyothant2593@gmail.com>
2026-04-01 20:16:51 -0700

    [70] [Time Beats: 100.00%] [Memory Beats: 91.78%] - LeetPush

--- Climbing-Stairs.py
+++ Climbing-Stairs.py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def climbStairs(self, n):
-        if n == 1:
-            return 1
-        dp = [0 for _ in range(n+1)]
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+1class Solution:
+2    def climbStairs(self, n: int) -> int:
+3        if n == 1:
+4            return 1
+5        
+6        dp = [0 for _ in range(n)]
+7        dp[0] = 1 # 1 step
+8        dp[1] = 2 # 2 step
+9        for i in range(2, n):
+10            dp[i] = dp[i - 1] + dp[i - 2] # 1 step or 2 step
+11        return dp[n - 1]
