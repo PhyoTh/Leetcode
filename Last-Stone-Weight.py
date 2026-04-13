af590afd3f95a3f02444435fea138de3e979d49b
Phyo Thant <phyothant2593@gmail.com>
2026-04-13 13:08:42 -0700

    [1046] [Time Beats: 100.00%] [Memory Beats: 46.56%] - LeetPush

--- Last-Stone-Weight.py
+++ Last-Stone-Weight.py
@@ -1,13 +1,15 @@
 1class Solution:
-2    # brute-force, N^2
-3    import bisect
-4    def lastStoneWeight(self, stones: List[int]) -> int:
-5        stones.sort() # nlogn
-6
-7        while len(stones) > 1: # n (worst case: O(15))
-8            t1, t2 = stones.pop(), stones.pop()
-9            t3 = t1 - t2
-10            bisect.insort(stones, t3)
-11        return stones[0]
-12    
-13
+2    import heapq
+3    def lastStoneWeight(self, stones: List[int]) -> int:
+4        for i in range(len(stones)): # n
+5            stones[i] *= -1
+6        heapq.heapify(stones)
+7
+8        while len(stones) > 1: # n
+9            t1, t2 = heapq.heappop(stones), heapq.heappop(stones) # logn
+10            t3 = t1 - t2
+11            if t3 != 0:
+12                heapq.heappush(stones, t3)
+13        
+14        return -1 * heapq.heappop(stones) if stones else 0
+15    
