abd8854d2a069361da9b63491fcb2b7c63dfaf03
Phyo Thant <phyothant2593@gmail.com>
2026-06-27 23:31:30 -0700

    [567] [Time Beats: 51.26%] [Memory Beats: 52.93%] - LeetPush

--- Permutation-in-String.py
+++ Permutation-in-String.py
@@ -2,14 +2,23 @@
 2class Solution:
 3    def checkInclusion(self, s1: str, s2: str) -> bool:
 4        n = len(s1)
-5        s1_dict = Counter(s1)
-6        
-7        left, right = 0, n
-8        while right <= len(s2):
-9            s2_dict = Counter(s2[left:right])
-10            if s1_dict == s2_dict:
-11                return True
-12            left += 1
-13            right += 1
-14
-15        return False
+5        if n > len(s2):
+6            return False
+7
+8        s1_dict = Counter(s1)
+9        s2_dict = {}
+10        for i in range(n):
+11            s2_dict[s2[i]] = s2_dict.get(s2[i], 0) + 1
+12
+13        left, right = 0, n
+14        while right < len(s2):
+15            if s1_dict == s2_dict:
+16                return True
+17            s2_dict[s2[left]] -= 1
+18            if s2_dict[s2[left]] == 0:
+19                del s2_dict[s2[left]]
+20            left += 1
+21            s2_dict[s2[right]] = s2_dict.get(s2[right], 0) + 1
+22            right += 1
+23
+24        return s1_dict == s2_dict
