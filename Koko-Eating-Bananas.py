f24234b47b0ac94f855f53308d28f6d7204b4641
Phyo Thant <phyothant2593@gmail.com>
2026-07-02 03:46:47 -0700

    [875] [Time Beats: 42.99%] [Memory Beats: 41.81%] - LeetPush

--- Koko-Eating-Bananas.py
+++ Koko-Eating-Bananas.py
@@ -1,19 +1,19 @@
-class Solution:
-    import math
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        left = 1
-        right = max(piles)
-
-        while left < right:
-            middle = left + (right - left) // 2
-
-            time_count = 0
-            for pile in piles:
-                time_count += math.ceil(pile / middle)
-            
-            if time_count <= h:
-                right = middle
-            else:
-                left = middle + 1
-        
-        return right
+1import math
+2class Solution:
+3    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+4        result = max(piles)
+5        left, right = 1, max(piles)
+6        while left < right:
+7            mid = (left + right) // 2
+8
+9            time = 0
+10            for pile in piles:
+11                time += math.ceil(pile / mid)
+12
+13            if time <= h:
+14                result = min(result, mid)
+15                right = mid
+16            else:
+17                left = mid + 1
+18
+19        return result
