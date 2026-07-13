9115765911e032089f8b38ed892fc4e25630dd6a
Phyo Thant <phyothant2593@gmail.com>
2026-07-12 17:20:18 -0700

    [347] [Time Beats: 24.73%] [Memory Beats: 12.46%] - LeetPush

--- 347-Top-K-Frequent-Elements.py
+++ 347-Top-K-Frequent-Elements.py
@@ -1,8 +1,25 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        res = []
-        x = Counter(nums) # Count frequency of the string
-        topK = x.most_common(k) # get the K most freequent elements
-        for num, freq in topK:
-                res.append(num)
-        return res
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        O(nlogn) Sol 1: Sort nums and count
+        O(nlogn) Sol 2: Get nums count and sort
+        O(klogn) Sol: use Heap and pop
+        '''
+        count = {}
+        for num in nums: # O(n)
+            count[num] = count.get(num, 0) + 1
+
+        freq = [[] for _ in range(len(nums) + 1)]
+        for num, count in count.items(): # O(n)
+            freq[count].append(num)
+        
+        result = []
+        for i in range(len(freq) - 1, 0, -1): # O(n)
+            while len(freq[i]) > 0 and k > 0:
+                temp = freq[i].pop()
+                result.append(temp)
+                k -= 1
+            
+            if k == 0:
+                break
+        return result
