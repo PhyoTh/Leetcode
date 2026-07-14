a46d41ac9c9420bfc1e9db1c450519b3b10f83ef
Phyo Thant <phyothant2593@gmail.com>
2026-07-14 16:58:45 -0700

    [104] [Time Beats: 100.00%] [Memory Beats: 19.70%] - LeetPush

---
+++ 104-Maximum-Depth-of-Binary-Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        result = float('-inf')
+        stk = [(root, 1)]
+        while stk:
+            node, depth = stk.pop()
+            result = max(result, depth)
+            
+            if node.right:
+                stk.append((node.right, depth + 1))
+            if node.left:
+                stk.append((node.left, depth + 1))
+
+        return result
