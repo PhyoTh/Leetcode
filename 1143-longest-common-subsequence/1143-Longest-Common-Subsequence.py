e83aa4d563d0a8f1c18813e7478b58faaf48df6a
Phyo Thant <phyothant2593@gmail.com>
2026-08-17 15:31:05 -0700

    [1143] [Time Beats: 61.75%] [Memory Beats: 25.96%] - LeetPush

--- 1143-longest-common-subsequence/1143-Longest-Common-Subsequence.py
+++ 1143-longest-common-subsequence/1143-Longest-Common-Subsequence.py
@@ -1,19 +1,11 @@
-from functools import lru_cache
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n1, n2 = len(text1), len(text2)
+        dp = [[0 for _ in range(len(text1) + 1)] for _ in range(len(text2) + 1)]
 
-        @lru_cache(maxsize=None)
-        def topdown(p1: int, p2: int) -> int:
-            if p1 == n1 or p2 == n2:
-                return 0
-            
-            case1 = topdown(p1 + 1, p2)
-            
-            case2 = 0
-            found = text2.find(text1[p1], p2)
-            if found != -1:
-                case2 = 1 + topdown(p1 + 1, found + 1)
-            return max(case1, case2)
-        
-        return topdown(0, 0)
+        for col in range(len(text1) - 1, -1, -1):
+            for row in range(len(text2) - 1, -1, -1):
+                if text1[col] == text2[row]:
+                    dp[row][col] = 1 + dp[row + 1][col + 1]
+                else:
+                    dp[row][col] = max(dp[row + 1][col], dp[row][col + 1])
+        return dp[0][0]
