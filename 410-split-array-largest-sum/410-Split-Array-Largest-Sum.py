c3ad90baf664383680d3d0a8025e7c39d2c20298
Phyo Thant <phyothant2593@gmail.com>
2026-08-22 11:25:25 -0700

    [410] [Time Beats: 79.13%] [Memory Beats: 82.39%] - LeetPush

--- 410-split-array-largest-sum/410-Split-Array-Largest-Sum.py
+++ 410-split-array-largest-sum/410-Split-Array-Largest-Sum.py
@@ -1,24 +1,25 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        cache = {}
+        left, right = max(nums), sum(nums)
+        result = right
 
-        def backtrack(start: int, split: int) -> int:
-            if split == 1:
-                return sum(nums[start:])
-            if (start, split) in cache:
-                return cache[(start, split)]
-            
-            cur_sum, min_result = 0, float('inf')
-            for i in range(start, n - split + 1):
-                cur_sum += nums[i]
-                max_sum = max(cur_sum, backtrack(i + 1, split - 1))
-                min_result = min(min_result, max_sum)
-
-                if cur_sum > min_result:
-                    break
-
-            cache[(start, split)] = min_result
-            return min_result
+        def valid(target: int) -> bool:
+            subarrays = 1
+            total = 0
+            for num in nums:
+                total += num
+                if total > target:
+                    subarrays += 1
+                    total = num
+            return subarrays <= k
         
-        return backtrack(0, k)
+        while left <= right:
+            mid = (left + right) // 2
+
+            if valid(mid):
+                result = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return result
