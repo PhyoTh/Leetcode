6658b1bdf23fdf5baaea0c9a7a9d47a5f7be0b7f
Phyo Thant <phyothant2593@gmail.com>
2026-09-07 14:23:05 -0500

    [252] [Time Beats: 15.18%] [Memory Beats: 40.88%] - LeetPush

---
+++ 252-Meeting-Rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) == 0:
+            return True
+        
+        intervals.sort()
+        prevEnd = intervals[0][1]
+
+        for start, end in intervals[1:]:
+            if start < prevEnd:
+                return False
+            prevEnd = end
+        return True
