216f84939bc417ebe5cb89885179cd7f5f0fff00
Phyo Thant <phyothant2593@gmail.com>
2026-09-11 15:34:27 -0500

    [642] [Time Beats: 90.96%] [Memory Beats: 59.10%] - LeetPush

--- 642-Design-Search-Autocomplete-System.py
+++ 642-Design-Search-Autocomplete-System.py
@@ -1,38 +1,59 @@
+import bisect
 class TrieNode:
     def __init__(self, c: str):
         self.c = c
         self.next = {}
-        self.indicies = []
+        self.sentences = []
 
 class AutocompleteSystem:
     def __init__(self, sentences: List[str], times: List[int]):
-        self.bank = list(zip(sentences, times))
-        self.bank.sort(key=lambda x: (-x[1], x[0]))
+        bank = list(zip(sentences, times))
+        bank.sort(key=lambda x: (-x[1], x[0]))
+        self.freq = {}
         self.root = TrieNode("")
-        self.walker = self.root
 
-        for i in range(len(self.bank)):
+        for i in range(len(bank)):
             walker = self.root
-            for c in self.bank[i][0]:
+            for c in bank[i][0]:
                 if c not in walker.next:
                     walker.next[c] = TrieNode(c)
                 walker = walker.next[c]
-                walker.indicies.append(i)
+                walker.sentences.append(bank[i][0])
+            
+            self.freq[bank[i][0]] = bank[i][1]
+        
+        self.walker = self.root
+        self.inputStr = []
+    
+    def update(self) -> None:
+        searchStr = ''.join(self.inputStr)
+        self.freq[searchStr] = self.freq.get(searchStr, 0) + 1
+
+        walker = self.root
+        for c in self.inputStr:
+            if c not in walker.next:
+                walker.next[c] = TrieNode(c)
+            walker = walker.next[c]
+            if searchStr not in walker.sentences:
+                walker.sentences.append(searchStr)
+            walker.sentences.sort(key=lambda x:(-(self.freq[x]), x))
+        
+        self.inputStr = []
+        self.walker = self.root
 
     def input(self, c: str) -> List[str]:
-        if c not in self.walker.next:
+        if c == '#':
+            self.update()
             return []
-        elif c == '#':
-            self.walker = self.root
-            return []
+        
+        self.inputStr.append(c)
 
+        if self.walker is None or c not in self.walker.next:
+            self.walker = None
+            return []
+        
         self.walker = self.walker.next[c]
-        result = []
-        for index in self.walker.indicies:
-            result.append(self.bank[index][0])
-            if len(result) == 3:
-                break
-        return result
+        return self.walker.sentences[:min(3, len(self.walker.sentences))]
 
 # Your AutocompleteSystem object will be instantiated and called as such:
 # obj = AutocompleteSystem(sentences, times)
