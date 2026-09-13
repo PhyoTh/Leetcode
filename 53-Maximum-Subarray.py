5a2afd8fe42b4a57326ebb01f3379a23f41ee0a2
Phyo Thant <phyothant2593@gmail.com>
2026-09-13 15:13:14 -0500

    [53] [Time Beats: 61.70%] [Memory Beats: 20.80%] - LeetPush

--- 53-Maximum-Subarray.py
+++ 53-Maximum-Subarray.py
@@ -1,25 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        def solve(left: int, right: int) -> int:
-            if left == right:
-                return nums[left]
-            
-            mid = (left + right) // 2
-            left_max = solve(left, mid)
-            right_max = solve(mid + 1, right)
-            
-            left_sum = 0
-            best_left = float('-inf')
-            for i in range(mid, left - 1, -1):
-                left_sum += nums[i]
-                best_left = max(best_left, left_sum)
+        current_sum = best_sum = nums[0]
 
-            right_sum = 0
-            best_right = float('-inf')
-            for i in range(mid + 1, right + 1):
-                right_sum += nums[i]
-                best_right = max(best_right, right_sum)
-
-            return max(left_max, right_max, best_left + best_right)
-
-        return solve(0, len(nums) - 1)
+        for i in range(1, len(nums)):
+            current_sum += nums[i]
+            if current_sum < nums[i]:
+                current_sum = nums[i]
+            best_sum = max(best_sum, current_sum)
+        
+        return best_sum
